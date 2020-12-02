81c69d69436eb419c08d8dcc3ddecef6019c9eb2
seekji <denibasov@yandex.ru>
2020-12-03 00:42:48 +0300

    fix: remove required option

--- packages/seo-bundle/Admin/RuleAdmin.php
+++ packages/seo-bundle/Admin/RuleAdmin.php
@@ -54,7 +54,7 @@ class RuleAdmin extends AbstractAdmin
             ->with('Основные настройки правила')
             ->add('pattern', UrlPatternType::class, ['sonata_help' => $this->getPatternHelp()])
             ->add('title', TextType::class, ['required' => false, 'sonata_help' => 'Заголовок сайта, отображается в тегах &lt;title&gt;&lt;/title&gt;'])
-            ->add('entity', TextType::class, ['sonata_help' => 'Необходимо указать для динамических свойств.'])
+            ->add('entity', TextType::class, ['sonata_help' => 'Необходимо указать для динамических свойств.', 'required' => false])
             ->add('locale', TextType::class, ['sonata_help' => 'Принимает два значения: en|ru.'])
             ->add('priority', TextType::class, ['sonata_help' => 'Приоритет правила. Если url соответстует нескольким правилам, то применяется правило с наибольшим приоритетом'])
             ->end()

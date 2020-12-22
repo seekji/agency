22a15a62387d028649fa8660bf3ea08e2f522939
seekji <denibasov@yandex.ru>
2020-12-22 19:54:35 +0300

    fix: add sort for services

--- src/Service/ServService.php
+++ src/Service/ServService.php
@@ -26,7 +26,7 @@ class ServService
 
     public function findServices(ListRequest $request): ListResponse
     {
-        $response = new ListResponse($this->serviceRepository->findBy(['locale' => $request->locale], null, $request->limit, $request->offset));
+        $response = new ListResponse($this->serviceRepository->findBy(['locale' => $request->locale], ['sort' => 'ASC'], $request->limit, $request->offset));
 
         return $response;
     }
